bea643f29bfa27a0c519ad6ec66f6314146af3b0
TeddyGavi <matc.davis@gmail.com>
2023-11-22 00:14:53 -0800

    working through testing

--- src/consumption/consumption.controller.spec.ts
+++ src/consumption/consumption.controller.spec.ts
@@ -1,12 +1,14 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { ConsumptionController } from './consumption.controller';
-import { ConsumptionService } from './consumption.service';
+import { Test, TestingModule } from "@nestjs/testing";
+import { ConsumptionController } from "./consumption.controller";
+import { ConsumptionService } from "./consumption.service";
+import { ConsumptionModule } from "./consumption.module";
 
-describe('ConsumptionController', () => {
+describe("ConsumptionController", () => {
   let controller: ConsumptionController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [ConsumptionModule],
       controllers: [ConsumptionController],
       providers: [ConsumptionService],
     }).compile();
@@ -14,7 +16,7 @@ describe('ConsumptionController', () => {
     controller = module.get<ConsumptionController>(ConsumptionController);
   });
 
-  it('should be defined', () => {
+  it("should be defined", () => {
     expect(controller).toBeDefined();
   });
 });
--- src/consumption/consumption.service.spec.ts
+++ src/consumption/consumption.service.spec.ts
@@ -1,14 +1,20 @@
 import { Test, TestingModule } from "@nestjs/testing";
 import { ConsumptionService } from "./consumption.service";
-
+import { ConsumptionModule } from "./consumption.module";
+import { Consumption } from "./entities/consumption.entity";
+import { ConsumptionController } from "./consumption.controller";
+import { UsersService } from "src/users/users.service";
+import { PizzasService } from "src/pizzas/pizzas.service";
+import { Repository } from "typeorm";
 describe("ConsumptionService", () => {
   let service: ConsumptionService;
+  let repository: Repository<Consumption>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ConsumptionService],
+      controllers: [ConsumptionController],
+      providers: [UsersService, PizzasService, ConsumptionModule],
     }).compile();
-
     service = module.get<ConsumptionService>(ConsumptionService);
   });
 
